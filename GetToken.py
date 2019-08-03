35b3ff4ae8688069ecffd21cc3755ca25f35cfe2
WHITEGHOSTS <40593639+WHITEGHOSTS@users.noreply.github.com>
2019-08-03 13:54:09 +0800

    Update GetToken.py

--- GetToken.py
+++ GetToken.py
@@ -1,7 +1,6 @@
 import urllib.request
 import json
 
-# 网址 https://console.bce.baidu.com/ai/#/ai/unit/app/list
 # client_id 为官网获取的AK， client_secret 为官网获取的SK
 host = 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=your AK&client_secret=your SK'
 request = urllib.request.Request(host)
